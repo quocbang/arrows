e30e83edfc414c06052f86c74055585e329961b0
bangquoc9 <bangquoc9@gmail.com>
2023-06-06 17:25:12 +0700

    read flag

---
+++ config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import "time"
+
+// FlagOptions definition.
+type FlagOptions struct {
+	Host    string    `long:"host" description:"" default:"localhost" env:"ARROWS_HOST"`
+	Port    string    `long:"port" description:"" default:"9091" env:"ARROWS_PORT"`
+	DevMode bool      `long:"dev-mode" description:"" env:"ARROWS_DEV_MODE"`
+	AuthKey string    `long:"auth-key" description:"" env:"ARROWS_AUTH_KEY"`
+	Timeout time.Time `long:"timeout" description:"request timeout, need to fetch unit time" default:"1m" env:"ARROWS_TIMEOUT"`
+	Config  string    `long:"config" env:"ARROWS_CONFIG"`
+}
+
+// TLSOptionsType definition.
+type TLSOptionsType struct {
+	Cert   string `long:"tls-cert" description:"path to TLS certificate (PUBLIC). To enable TLS handshake, you must set this value" env:"ARROWS_TLS_CERT"`
+	Key    string `long:"tls-key" description:"path to TLS certificate key (PRIVATE), To enable TLS handshake, you must set this value" env:"ARROWS_TLS_KEY"`
+	RootCA string `long:"root-ca" description:"path to the root certificate"`
+}
+
+// UseTLS check whether use tls mode or not.
+func (t TLSOptionsType) UseTLS() bool {
+	return t.Cert != "" && t.Key != ""
+}
+
+// Config difinition.
+type Config struct {
+	PostGres *PostGesConfig `yaml:"postgres"`
+	Cloud    *CloudConfig   `yaml:"cloud"`
+	Redis    *RedisConfig   `yaml:"redis"`
+}
+
+// PostGesConfig connection info.
+type PostGesConfig struct {
+	Schema   string `yaml:"schema"`
+	Name     string `yaml:"name"`
+	Address  string `yaml:"address"`
+	Port     int    `yaml:"port"`
+	UserName string `yaml:"userName"`
+	Password string `yaml:"password"`
+}
+
+// Cloud connection info.
+// TODO: needs cloud account.
+type CloudConfig struct {
+	Schema   string `yaml:"schema"`
+	Name     string `yaml:"name"`
+	Address  string `yaml:"address"`
+	Port     int    `yaml:"port"`
+	UserName string `yaml:"userName"`
+	Password string `yaml:"password"`
+}
+
+// Redis connection info.
+type RedisConfig struct {
+}
---
+++ go.mod
@@ -0,0 +1,22 @@
+module github.com/quocbang/arrows
+
+go 1.20
+
+require (
+	github.com/go-openapi/swag v0.22.3
+	github.com/golang/protobuf v1.5.3
+	github.com/jessevdk/go-flags v1.5.0
+	google.golang.org/grpc v1.55.0
+)
+
+require (
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
